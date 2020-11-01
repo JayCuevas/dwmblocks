f6746420f7247e900bd5659c3eb8370bf1b54ea0
JayCuevas <Jordan.Cuevas@precisecal.com>
2020-10-31 21:36:10 -0400

    Updated blocks

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,20 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	/* {"⌨", "kbselect", 0, 30}, */
+	// {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	/* {"",	"music",	0,	11}, */
+	{"",	"pacpackages",	0,	8},
+	/* {"",	"torrent",	20,	7}, */
+	{"",	"memory",	2,	14}, 
+	// {"",	"cpu",		10,	18}, 
+	{"",	"cpubars",		1,	18}, 
+	{"",	"nettraf",	1,	16},
+	{"",	"volume",	0,	10},
+	// {"",	"battery",	5,	3},
+	{"",	"internet",	5,	4},
+	{"",	"clock",	60,	1},
+	// {"",	"help-icon",	0,	15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
