fee62487b7e6d28b700e5334fe50ac260d57f26c
Jay <Jordan.Cuevas@precisecal.com>
2020-11-03 12:54:52 -0500

    Update dwmblocks config

--- blocks.def.h
+++ blocks.def.h
@@ -4,14 +4,15 @@ static const Block blocks[] = {
 	/* {"⌨", "kbselect", 0, 30}, */
 	// {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
+	{"",	"pacpackages",	120,	8},
 	/* {"",	"torrent",	20,	7}, */
+	{"",	"basename \"$(cat ~/scripts/wallpaper/current)\"",	5,	14}, 
 	{"",	"memory",	2,	14}, 
 	// {"",	"cpu",		10,	18}, 
 	{"",	"cpubars",		1,	18}, 
 	{"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
-	// {"",	"battery",	5,	3},
+	{"",	"battery",	5,	3},
 	{"",	"internet",	5,	4},
 	{"",	"clock",	60,	1},
 	// {"",	"help-icon",	0,	15},
